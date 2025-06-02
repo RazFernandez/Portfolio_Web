550fcc76ce28218f705ba82adc15e1d629ef4767
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-02 16:30:28 -0500

    Added JS structure to run scripts for webpage behaviour

---
+++ main.js
@@ -0,0 +1,9 @@
+// This script add functionality to navbar
+function displayBurgerMenu() {
+    console.log("Eureka!!!");
+    // const navbar_burger_bottom = document.getElementById("navbar-default");
+    // navbar.classList.toggle("hidden");
+    // // if (navbar_burger_bottom.className === "hidden") {
+        
+    // // }
+}
